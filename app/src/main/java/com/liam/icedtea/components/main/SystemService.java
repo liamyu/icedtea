9abcbb29a6901a954aafb058b2be64a5e05e0392
liamyu <ylc@cang.com>
2022-01-11 16:37:40 +0800

    feat(aar):  新增aar打包专用module

--- app/src/main/java/com/liam/icedtea/components/main/SystemService.java
+++ app/src/main/java/com/liam/icedtea/components/main/SystemService.java
@@ -1,18 +1,18 @@
-package com.liam.icedtea.components.main;
-
-import com.cang.Api;
-import com.cang.Cmd;
-import com.cang.Host;
-import java.util.Date;
-
-/**
- * 系统请求. Created by Liam on 2017/3/28.
- */
-@Api
-public interface SystemService {
-
-  @Host
-  @Cmd
-  Date getTime();
-
-}
+//package com.liam.icedtea.components.main;
+//
+//import com.cang.Api;
+//import com.cang.Cmd;
+//import com.cang.Host;
+//import java.util.Date;
+//
+///**
+// * 系统请求. Created by Liam on 2017/3/28.
+// */
+//@Api
+//public interface SystemService {
+//
+//  @Host
+//  @Cmd
+//  Date getTime();
+//
+//}
