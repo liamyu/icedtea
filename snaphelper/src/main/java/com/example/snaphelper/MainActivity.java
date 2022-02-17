82c763b13ba78c79406cfb88f265cda9df2b6ac0
liamyu <ylc@cang.com>
2022-02-17 17:49:10 +0800

    feat(snaphelper): 新增snaphelper模块

---
+++ snaphelper/src/main/java/com/example/snaphelper/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.snaphelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    RecyclerView recyclerView = findViewById(R.id.rv);
+    recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+  }
+}
